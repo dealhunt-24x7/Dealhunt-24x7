0794167928f971ea83fd429dc2877fdf9f34cfe3
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-08-29 10:55:12 +0530

    Add files via upload

---
+++ app/products/page.js
@@ -0,0 +1,12 @@
+import ProductCard from '../../components/ProductCard';
+
+export default function ProductsPage() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">All Products</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {/* Add more ProductCards here */}
+      </div>
+    </div>
+  );
+}
