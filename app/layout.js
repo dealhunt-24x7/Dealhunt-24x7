e89feb7803d512873ad00a43cee6b042b05be5e4
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-08-29 23:34:19 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -9,16 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head />
-      <body>
-        <header className="header">
-          <div className="brand-name">DealHunt</div>
-          {/* Navbar placeholder */}
-        </header>
-        <main>{children}</main>
-        <footer className="footer">
-          © 2025 DealHunt. All rights reserved.
-        </footer>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
