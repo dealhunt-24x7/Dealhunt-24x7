9c38b58a42fe39106eac29d92f482e874880764b
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-08-29 10:56:17 +0530

    Add files via upload

---
+++ app/about/page.js
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">About Dealhunt</h1>
+      <p>Dealhunt.in is a product comparison & deals aggregator platform.</p>
+    </div>
+  );
+}
