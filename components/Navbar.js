39a7be2cc07e2c4b2c2de1f94f7db5cca2c6980c
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-08-30 22:26:56 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,10 +7,9 @@ export default function Navbar() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const menuRef = useRef();
 
-  // Close sidebar if clicked outside
   useEffect(() => {
-    function handleClickOutside(event) {
-      if (menuRef.current && !menuRef.current.contains(event.target)) {
+    function handleClickOutside(e) {
+      if (menuRef.current && !menuRef.current.contains(e.target)) {
         setSidebarOpen(false);
       }
     }
@@ -20,7 +19,6 @@ export default function Navbar() {
 
   return (
     <nav className="top-navbar">
-      {/* Left - Hamburger */}
       <div className="flex items-center gap-2">
         <button
           className="hamburger md:hidden flex flex-col justify-between h-5 w-6"
@@ -33,18 +31,15 @@ export default function Navbar() {
         <h1 className="brand-name">DealHunt</h1>
       </div>
 
-      {/* Center Search */}
       <div className="searchbar">
         <input type="text" placeholder="Search products..." />
       </div>
 
-      {/* Right */}
       <div className="top-right">
         <img src="/profile.jpg" alt="Profile" className="profile-icon" />
         <button className="topbtn">Menu</button>
       </div>
 
-      {/* Sidebar */}
       <div ref={menuRef}>
         <Sidebar open={sidebarOpen} setOpen={setSidebarOpen} />
       </div>
